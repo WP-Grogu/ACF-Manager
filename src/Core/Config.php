401852f028c864eba17fa3144dd0268b46d88a69
Thomas <themagnetiiktbk@gmail.com>
2022-04-19 10:47:19 +0200

    :bug: Remove private function __wakeup() to support php 8.x

--- src/Core/Config.php
+++ src/Core/Config.php
@@ -36,13 +36,6 @@ final class Config
     }
 
     /**
-     * prevent from being unserialized (which would create a second instance of it)
-     */
-    private function __wakeup()
-    {
-    }
-
-    /**
      * is not allowed to call from outside to prevent from creating multiple instances,
      * to use the singleton, you have to obtain the instance from Singleton::getInstance() instead
      */
